f178117749547969616eb67f7129e5926c10f9db
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-02-28 00:22:51 +0600

    main window was created

---
+++ main.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, **kwargs):
+        super(MainWindow, self).__init__(**kwargs)
+
+
+def catch_exceptions(t, val, tb):
+    QMessageBox.critical(None, 'An exception was raised', 'Exception type: {}'.format(t))
+    old_hook(t, val, tb)
+
+
+if __name__ == '__main__':
+    old_hook = sys.excepthook
+    sys.excepthook = catch_exceptions
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
