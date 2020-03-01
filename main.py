e4642d731b6113ce69103ea2d86d49cbf12806d5
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-03-01 17:17:48 +0600

    names were changed and labels were added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMessageBox, QPlainTextEdit, QPushButton
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMessageBox, QPlainTextEdit, QPushButton, QTableView
 import sys
 
 
@@ -13,9 +13,10 @@ class MainWindow(QMainWindow):
         self.infoText.resize(180, 40)
         self.infoText.move(70, 50)
 
-        self.enterPasswordText = QPlainTextEdit(self)
-        self.enterPasswordText.resize(180, 40)
-        self.enterPasswordText.move(70, 120)
+        self.enterPasswordInput = QPlainTextEdit(self)
+        self.enterPasswordInput.setPlaceholderText("Password...")
+        self.enterPasswordInput.resize(180, 40)
+        self.enterPasswordInput.move(70, 120)
 
         self.passwordButton = QPushButton(self)
         self.passwordButton.setText("Submit")
@@ -23,40 +24,56 @@ class MainWindow(QMainWindow):
         self.passwordButton.move(85, 190)
         self.passwordButton.clicked.connect(self.check_password)
 
-        self.loginText = QPlainTextEdit(self)
-        self.loginText.hide()
-        self.passwordText = QPlainTextEdit(self)
-        self.passwordText.hide()
-        self.websiteText = QPlainTextEdit(self)
+        self.websiteText = QLabel(self)
         self.websiteText.hide()
+        self.websiteInput = QPlainTextEdit(self)
+        self.websiteInput.hide()
+        self.loginText = QLabel(self)
+        self.loginText.hide()
+        self.loginInput = QPlainTextEdit(self)
+        self.loginInput.hide()
+        self.passwordText = QLabel(self)
+        self.passwordText.hide()
+        self.passwordInput = QPlainTextEdit(self)
+        self.passwordInput.hide()
         self.addButton = QPushButton(self)
-        self.addButton.setText("Add")
         self.addButton.hide()
 
     def check_password(self):
-        if self.enterPasswordText.toPlainText() == "qqq":
+        if self.enterPasswordInput.toPlainText() == "qqq":
             self.change_menu()
         else:
             self.infoText.setText("WRONG!")
 
     def change_menu(self):
+        self.resize(320, 480)
         self.infoText.hide()
-        self.enterPasswordText.hide()
+        self.enterPasswordInput.hide()
         self.passwordButton.hide()
 
         self.websiteText.show()
-        self.websiteText.resize(180, 40)
-        self.websiteText.move(70, 20)
+        self.websiteText.setText("Website:")
+        self.websiteText.move(20, 20)
+        self.websiteInput.show()
+        self.websiteInput.resize(180, 40)
+        self.websiteInput.move(120, 20)
 
         self.loginText.show()
-        self.loginText.resize(180, 40)
-        self.loginText.move(70, 70)
+        self.loginText.setText("Login:")
+        self.loginText.move(20, 70)
+        self.loginInput.show()
+        self.loginInput.resize(180, 40)
+        self.loginInput.move(120, 70)
 
         self.passwordText.show()
-        self.passwordText.resize(180, 40)
-        self.passwordText.move(70, 120)
+        self.passwordText.setText("Password:")
+        self.passwordText.move(20, 120)
+        self.passwordInput.show()
+        self.passwordInput.resize(180, 40)
+        self.passwordInput.move(120, 120)
 
         self.addButton.show()
+        self.addButton.setText("Add")
         self.addButton.resize(180, 40)
         self.addButton.move(70, 170)
 
